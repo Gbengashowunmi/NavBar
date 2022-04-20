3402377ab19e0751d2e0b914ddddcafd9778bcb5
Gbengashowunmi <96231197+Gbengashowunmi@users.noreply.github.com>
2022-04-20 23:05:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ const menuIcon = document.querySelector(".menu-icon");
 const menu = document.querySelector(".menu");
 const body = document.querySelector(".body");
 
-menuIcon.addEventListener("click", function(){
+menuIcon.addEventListener("click", ()=> {
     menuIcon.classList.toggle("active");
     menu.classList.toggle("active");
     
 })
 
-menu.addEventListener("click", function(){
+menu.addEventListener("click", ()=> {
     menu.classList.remove("active")
 
 })
